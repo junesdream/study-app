7854e9d4b3d6f5759e2fed08e9c1dd708b63e01f
June <norisori@gmail.com>
2026-04-25 16:16:58 +0200

    fix service worker caching

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,25 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("app-cache").then((cache) => {
+            return cache.addAll([
+                "/",
+                "/manifest.json",
+                "/icon-192.png",
+                "/icon-512.png"
+            ]);
+        })
+    );
     self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
     event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
